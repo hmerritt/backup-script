5e2892cb97bbcd8741211dc3602db230476558b1
Harry Merritt <harry@merritt.es>
2020-04-04 04:34:17 +0100

    Add cli.sh module
    - handles command-line interface
    - cli helper functions
    - colored text and logging

---
+++ src/modules/cli.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+
+# Color codes
+NOCOLOR="\033[0m"
+
+# Text
+BLACK="\e[30m"
+RED="\e[31m"
+GREEN="\e[32m"
+ORANGE="\e[33m"
+BLUE="\e[34m"
+PURPLE="\e[35m"
+CYAN="\e[36m"
+LIGHTGRAY="\e[37m"
+DARKGRAY="\e[90m"
+LIGHTRED="\e[91m"
+LIGHTGREEN="\e[92m"
+YELLOW="\e[93m"
+LIGHTBLUE="\e[94m"
+LIGHTPURPLE="\e[95m"
+LIGHTCYAN="\e[96m"
+WHITE="\e[97m"
+
+# Background
+BG_RED="\e[41m${WHITE}"
+BG_GREEN="\e[42m${WHITE}"
+BG_YELLOW="\e[43m${WHITE}"
+BG_BLUE="\e[44m${WHITE}"
+BG_PURPLE="\e[45m${WHITE}"
+BG_CYAN="\e[46m${WHITE}"
+BG_LIGHTRED="\e[101m${WHITE}"
+BG_LIGHTGREEN="\e[102m${WHITE}"
+BG_WHITE="\e[107m${WHITE}"
+
+
+##-----------------------------------------------
+
+
+# Print colored text to the terminal
+cprint () {
+	echo -e "$1$2${NOCOLOR}"
+}
+
+
+##-----------------------------------------------
+
+
+# Print success/failure message
+# -> output has green/red Background with white text
+success () {
+	cprint $BG_GREEN "${1}"
+}
+
+failure () {
+	cprint $BG_RED "${1}"
+}
+
+
+##-----------------------------------------------
