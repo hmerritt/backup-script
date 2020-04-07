e049438819484aba23dc76658f9e52ed1e363996
Harry Merritt <harry@merritt.es>
2020-04-07 04:05:10 +0100

    Add helper functions to files.sh
    - check if a file exists
    - check if a directory exists

--- src/modules/files.sh
+++ src/modules/files.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
 
-# Move a file to another location
-# - $1: path to original file
-# - $2: path of new file
+## Move a file to another location
+## - $1: path to original file
+## - $2: path of new file
 move () {
 	mv "${1}" "${2}" --verbose
+}
+
+
+## Check if a file exists
+## -$1: path to file
+isfile () {
+	if [ -f "${1}" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+
+## Check if a directory exists
+## -$1: path to directory
+isdirectory () {
+	if [ -d "${1}" ]; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 
 ##------------------------------------------------------------------------------
 
 
-# Create a compressed tar (uses parallel cpu multi-threading)
-# - $1: name of new tar
-# - $2: path of file/directory to tar
+## Create a compressed tar (uses parallel cpu multi-threading)
+## - $1: name of new tar
+## - $2: path of file/directory to tar
 compress () {
 	tar --use-compress-program="pigz --best --recursive --no-time" -cvf "${1}.tar.gz" "${2}"
 }
@@ -23,10 +45,10 @@ compress () {
 ##------------------------------------------------------------------------------
 
 
-# Backup individual file/folder
-# - $1: name of item to backup
-# - $2: directory of the item to backup
-# - $3: directory of the backup location
+## Backup individual file/folder
+## - $1: name of item to backup
+## - $2: directory of the item to backup
+## - $3: directory of the backup location
 backup () {
 	action "Backing up ${1}"
 
