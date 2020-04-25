234f188da65ff2397ed3108d0b812c487cf64c6a
Harry Merritt <harry@merritt.es>
2020-04-25 05:23:13 +0100

    Improve code comments for main backup function

--- src/modules/files.sh
+++ src/modules/files.sh
@@ -40,27 +40,34 @@ isdirectory () {
 ## - $3: directory of the backup location
 backup () {
 
-	##  Get args | use fallback values
+	## Get args | use fallback values
 	item_name=$(fallback "${1}" "")
 	item_dir_local=$(fallback "${2}" "/")
 	item_dir_backup=$(fallback "${3}" "${item_dir_local}")
 
 	action "Backing up ${item_name}"
 
+	## Set tmp folder location
 	local tmp_folder="/var/tmp"
 	local tmp_file="${tmp_folder}/${item_name}"
 
+	## If root folder exists
+	## Use absolute file paths
 	if [ "${DIR_ROOT_LOCAL}" != "" ] || [ "${item_dir_local}" != "" ]; then
-		cd "${DIR_ROOT_LOCAL}${item_dir_local}" || onfail "" "Error opening directory '${DIR_ROOT_LOCAL}${item_dir_local}'"
+		cd "${DIR_ROOT_LOCAL}${item_dir_local}" || \
+		   onfail "" "Error opening directory '${DIR_ROOT_LOCAL}${item_dir_local}'"
 	fi
 
+	## Tar.gz item
 	task "Compressing ${item_name}"
 	compress "${tmp_file}" "${item_name}" & spinner
 	onfail
 
+	## Move item from tmp/ to backup location
 	task "Moving ${item_name} to backup location"
 	move "${tmp_file}.tar.gz" "${DIR_ROOT_BACKUP}${item_dir_backup}${item_name}.tar.gz" & spinner
 	onfail
 
 	green "Completed: ${item_name}"
+
 }
